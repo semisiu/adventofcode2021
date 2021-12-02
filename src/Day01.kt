6297b97695a794e8aa0ecc8b62c250e0331e4566
Michal Sulik <michal.sulik@allegro.pl>
2021-12-02 21:44:37 +0100

    Day01 solutions

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,27 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .asSequence()
+            .map { Integer.valueOf(it) }
+            .zipWithNext()
+            .count { (x, y) -> y > x }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .asSequence()
+            .map { Integer.valueOf(it) }
+            .zipWithNext()
+            .zipWithNext()
+            .map { (p1, p2) -> p1.first + p1.second + p2.second }
+            .zipWithNext()
+            .count { (x, y) -> y > x }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
